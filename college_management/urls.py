a3e75a1c18a4b3d22660d732add82707b25da84c
Kanishkumar <kanishkumar.24cs@licet.ac.in>
2024-11-20 19:25:21 +0530

    Update urls.py

--- college_management/urls.py
+++ college_management/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from college import views 
 
 urlpatterns = [
+    path('', views.home, name='home'),  # This will render the home page
     path('admin/', admin.site.urls),
     path('api/', include('college.urls')), 
 ]
